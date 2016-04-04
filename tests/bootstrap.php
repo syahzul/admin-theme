bd0b38a7df93d95086dc4888f4b47f2bd3c5fb9d
Syahril Zulkefli <syahzul@gmail.com>
2016-04-04 15:16:35 +0800

    Added test file

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+ $loader = require __DIR__ . "/../vendor/autoload.php";
+ $loader->addPsr4('Syahzul\\AdminTheme\\Tests', __DIR__.'/AdminTheme');
+
+ date_default_timezone_set('Asia/Kuala_Lumpur');
