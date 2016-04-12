eb4e0392c39409252c9261e6d25ec50b62329bc9
Syahril Zulkefli <syahzul@gmail.com>
2016-04-12 23:14:45 +0800

    Change dashboard icon link and text

--- src/resources/views/partials/sidebar.blade.php
+++ src/resources/views/partials/sidebar.blade.php
@@ -31,7 +31,7 @@
         <ul class="sidebar-menu">
             <li class="header">HEADER</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>Home</span></a></li>
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-home'></i> <span>Dashboard</span></a></li>
             <li><a href="#"><i class='fa fa-link'></i> <span>Another Link</span></a></li>
             <li class="treeview">
                 <a href="#"><i class='fa fa-link'></i> <span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
@@ -40,6 +40,8 @@
                     <li><a href="#">Link in level 2</a></li>
                 </ul>
             </li>
+            <li class="header">ADMINISTRATIONS</li>
+            <li><a href="#"><i class='fa fa-users'></i> <span>User Manager</span></a></li>
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
