12849a412c9b351bece4c8521ad95344a056ea04
Syahril Zulkefli <syahzul@gmail.com>
2017-03-06 18:44:09 +0800

    Remove trait reference to detect namespace for Laravel 5.4

--- src/Commands/AdminThemeCommand.php
+++ src/Commands/AdminThemeCommand.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\File;
 
 class AdminThemeCommand extends Command
 {
-    use AppNamespaceDetectorTrait;
-
     /**
      * The name and signature of the console command.
      *
